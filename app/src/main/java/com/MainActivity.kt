c4ac273e05fe1637975adbd791db0146965ffe4d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-01-08 10:31:09 -0300

    workingOnActivityMain

--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -2,11 +2,27 @@ package com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adapter.MainAdapter
 import com.example.mvvmprojectstructure.R
+import com.example.mvvmprojectstructure.databinding.ActivityMainBinding
+import com.rest.RetrofitService
+import com.viewmodel.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    lateinit var viewModel: MainViewModel
+
+    private val  retrofitService = RetrofitService.getInstance()
+
+    private val adapter = MainAdapter{
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
