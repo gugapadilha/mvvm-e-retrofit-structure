35a6249461b831e993d0333fab5c67c46d9ab341
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-01-16 00:21:20 -0300

    finishedAdapterAlteration

--- app/src/main/java/com/repository/MainRepository.kt
+++ app/src/main/java/com/repository/MainRepository.kt
@@ -4,5 +4,7 @@ import com.rest.RetrofitService
 
 class MainRepository constructor(private val retrofitService: RetrofitService) {
 
+    //getAllLives recebendo o retrofitService (INTERFACE)
     fun getAllLives() = retrofitService.getAllLives()
+
 }
